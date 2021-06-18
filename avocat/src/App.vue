b4bc05f424c655082a8fa9cdadd8a887564a6a80
chaimaa nmiyesse <chaimaanamisse@gmail.com>
2021-06-18 18:49:38 +0100

    first commit

---
+++ avocat/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Bienvenue chez nous"/> -->
+  <firstForm/>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import firstForm from './components/firstForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    firstForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
---
+++ avocat/src/components/firstForm.vue
@@ -0,0 +1,87 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+      <label >Nom complet:</label>
+      <input type="text" required v-model="fullname">
+
+
+      <label >Carte d'identité nationale</label>
+      <input type="text" required v-model="cin">
+
+      <label >Numero de téléphone:</label>
+      <input type="text" required v-model="phonenumber">
+      
+      <div class="submit">
+          <button>Envoyer</button>
+      </div>
+
+
+      
+
+  </form>
+  
+  
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            fullname: '',
+            phonenumber: '',
+            cin: ''
+
+
+
+
+        }
+    },
+    methods: {
+    handleSubmit() {
+        console.log('form submitted')
+    }
+    }
+
+}
+</script>
+
+<style>
+form{
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
+label{
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+input{
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+}
+button {
+    background: #0b6dff;
+    border: 0;
+    padding:10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+}
+.submit {
+    text-align: center;
+}
+
+
+</style>
